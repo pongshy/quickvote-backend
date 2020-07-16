cc8cb06dd53db22997e70182ce256cb37ebcf53d
SoCMo <247805431@qq.com>
2020-07-16 17:56:33 +0800

    add: voteRecord

--- src/main/java/com/shu/votetool/controller/VoteController.java
+++ src/main/java/com/shu/votetool/controller/VoteController.java
@@ -22,35 +22,48 @@ public class VoteController {
     @Resource
     private VoteService voteService;
 
-    /** 
-    * @Description: 创建投票项目 
-    * @Param: [newVoteReq, openid] 
-    * @return: org.springframework.http.ResponseEntity<java.lang.Object> 
+    /**
+    * @Description: 创建投票项目
+    * @Param: [newVoteReq, openid]
+    * @return: org.springframework.http.ResponseEntity<java.lang.Object>
     * @Author: SoCMo
-    * @Date: 2020/7/16 
-    */ 
+    * @Date: 2020/7/16
+    */
     @PostMapping("/voteSystem")
     public ResponseEntity<Object> newVoteSystem(@Validated @RequestBody NewVoteReq newVoteReq,
                     @RequestHeader("openid") String openid){
         return voteService.createVoteSystem(newVoteReq, openid);
     }
 
-    /** 
-    * @Description: 获取投票列表 
-    * @Param: [voteSystemListReq, openid] 
-    * @return: org.springframework.http.ResponseEntity<java.lang.Object> 
+    /**
+    * @Description: 获取投票列表
+    * @Param: [voteSystemListReq, openid]
+    * @return: org.springframework.http.ResponseEntity<java.lang.Object>
     * @Author: SoCMo
-    * @Date: 2020/7/16 
-    */ 
+    * @Date: 2020/7/16
+    */
     @GetMapping("/voteSystemList")
     public ResponseEntity<Object> voteSystemList(@Validated VoteSystemListReq voteSystemListReq,
                                                  @RequestHeader("openid") String openid){
         return voteService.voteSystemList(voteSystemListReq, openid);
     }
 
+    /**
+    * @Description: 投票
+    * @Param: [voteReq, openid]
+    * @return: org.springframework.http.ResponseEntity<java.lang.Object>
+    * @Author: SoCMo
+    * @Date: 2020/7/16
+    */
+    @PostMapping("/voteRecord")
+    public ResponseEntity<Object> vote(@Validated @RequestBody VoteReq voteReq,
+                                                 @RequestHeader("openid") String openid){
+        return voteService.vote(voteReq, openid);
+    }
+
     /** 
     * @Description: 获取投票详情 
-    * @Param: [id, openid] 
+    * @Param: [id] 
     * @return: org.springframework.http.ResponseEntity<java.lang.Object> 
     * @Author: SoCMo
     * @Date: 2020/7/16 
@@ -58,18 +71,5 @@ public class VoteController {
     @GetMapping("/voteDetail")
     public ResponseEntity<Object> voteDetail(@RequestParam Integer id){
         return voteService.voteSystemDetail(id);
-    }
-
-    /** 
-    * @Description: 投票 
-    * @Param: [voteReq, openid] 
-    * @return: org.springframework.http.ResponseEntity<java.lang.Object> 
-    * @Author: SoCMo
-    * @Date: 2020/7/16 
-    */ 
-    @PostMapping("/voteRecord")
-    public ResponseEntity<Object> vote(@Validated @RequestBody VoteReq voteReq,
-                                       @RequestHeader("openid") String openid){
-        return voteService.vote(voteReq, openid);
     }
 }
